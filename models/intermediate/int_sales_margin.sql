ca99400c19ddd12428c28e1fa5580b73c6c7370d
Peter <tarrpete@gmail.com>
2025-05-13 14:37:47 +0000

    Added an intermediate model to calculate purchase cost and margin

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,14 @@
+select
+    date_date
+    ,orders_id
+    ,products_id
+    ,revenue
+    ,quantity
+    ,purchase_price
+    ,(quantity * purchase_price) as purchase_cost
+    ,(revenue - (quantity * purchase_price)) as margin
+from
+    {{ref('stg_raw__sales')}}
+    join 
+        {{ref('stg_raw__product')}}
+        using (products_id)
